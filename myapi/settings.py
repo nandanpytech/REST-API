cdd126099b42d68fa7598d4786d933a2ae88805d
nandanpytech <bilaginandan333@gmail.com>
2022-12-16 16:16:06 +0530

    change

--- myapi/settings.py
+++ myapi/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles','static')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
