2330d956152ec2c562bca7c4c2e1b88679006dc4
nandanpytech <bilaginandan333@gmail.com>
2022-12-16 16:56:10 +0530

    changes

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
-# echo "BUILD START"
-# python3.9 -m pip install -r requirements.txt
-# python3.9 manage.py collectstatic --noinput --clear
-# echo "BUILD END"
+echo "BUILD START"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo "BUILD END"
 
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+# pip install -r requirements.txt
+# python3.9 manage.py collectstatic
